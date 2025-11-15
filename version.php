40c1f73ad1dfb8dc8ff55159cbcc14600904926c
Carlo Comincini <carlo@comincini.it>
2025-11-15 23:33:00 +0100

    Fix syntax error in version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'scormvideomaker';
-$plugin->version   = 2025111501
+$plugin->version   = 2025111501;
 $plugin->release   = '1.0.0';
 $plugin->requires  = 2022041900; // Moodle 4.0+
 $plugin->maturity  = MATURITY_ALPHA;

287773036cbdb5702af08df9c9637a6d128c4b84
Carlo Comincini <carlo@comincini.it>
2025-11-16 00:03:45 +0100

    Fix: Corretto @package e aggiunte stringhe mancanti nel file di lingua italiana

--- lang/it/scormvideomaker.php
+++ lang/it/scormvideomaker.php
@@ -16,7 +16,7 @@
 /**
  * Language strings for SCORM Video Maker (Italian).
  *
- * @package scormvideomaker
+ * @package local_scormvideomaker
  * @copyright 2025 Carlo Comincini <carlo@comincini.it>
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
@@ -27,6 +27,7 @@ $string['pluginname'] = 'SCORM Video Maker';
 $string['plugindesc'] = 'Crea pacchetti SCORM da URL video (Vimeo, YouTube, HLS)';
 
 // Admin menu.
+$string['createscrorm'] = 'Crea pacchetto video SCORM';
 $string['createscorm'] = 'Crea pacchetto SCORM';
 
 // Settings.
@@ -38,49 +39,63 @@ $string['setting_default_completion_type'] = 'Tipo di completamento predefinito'
 $string['setting_default_completion_type_desc'] = 'Seleziona il tipo di completamento predefinito per i nuovi video';
 $string['setting_default_completion_percentage'] = 'Percentuale completamento predefinita';
 $string['setting_default_completion_percentage_desc'] = 'Percentuale predefinita di video da guardare prima di contrassegnarlo come completato (0-100)';
-$string['setting_scorm_settings_help'] = 'Configura le impostazioni standard dell\'attività SCORM che verrebbero aggiunte manualmente a un corso';
 
 // Form labels - Video Configuration.
 $string['form_course'] = 'Corso';
-$string['form_course_help'] = 'Seleziona il corso in cui verrà creato il pacchetto SCORM';
+$string['form_course_help'] = 'Seleziona il corso in cui verrà creata l\'attività SCORM';
 $string['form_title'] = 'Titolo attività';
-$string['form_title_help'] = 'Inserisci il titolo del pacchetto SCORM';
+$string['form_title_help'] = 'Inserisci il titolo dell\'attività SCORM';
 $string['form_description'] = 'Descrizione';
-$string['form_description_help'] = 'Inserisci una descrizione del pacchetto SCORM (supporta la formattazione del testo)';
+$string['form_description_help'] = 'Inserisci una descrizione dell\'attività SCORM (supporta la formattazione del testo)';
 $string['form_videotype'] = 'Tipo di video';
 $string['form_videotype_help'] = 'Seleziona il tipo di sorgente video: Vimeo, YouTube o HLS Stream';
-$string['form_videourl'] = 'URL video';
-$string['form_videourl_help'] = 'Per Vimeo: inserisci l\'URL completo o l\'ID video. Per YouTube: inserisci l\'URL completo o l\'ID video (ad es., dQw4w9WgXcQ). Per HLS: inserisci l\'URL completo del file M3U';
-$string['form_videowidth'] = 'Larghezza video';
-$string['form_videowidth_help'] = 'Larghezza del lettore video in pixel';
-$string['form_videoheight'] = 'Altezza video';
-$string['form_videoheight_help'] = 'Altezza del lettore video in pixel';
+$string['form_videourl'] = 'URL/Codice video';
+$string['form_videourl_help'] = 'Per Vimeo: inserisci l\'ID video (es. 123456789). Per YouTube: inserisci l\'ID video (es. dQw4w9WgXcQ). Per HLS: inserisci l\'URL completo del file M3U8';
+$string['form_seekbar'] = 'Comportamento barra di avanzamento (video incompleti)';
+$string['form_seekbar_help'] = 'Controlla come gli utenti possono navigare nei video non ancora completati';
+$string['form_completion_type'] = 'Tipo di completamento';
+$string['form_completion_type_help'] = 'Seleziona come l\'attività deve essere contrassegnata come completata';
+$string['form_completion_percentage'] = 'Percentuale di completamento';
+$string['form_completion_percentage_help'] = 'Inserisci la percentuale di video che deve essere guardata (0-100). Usato solo quando è selezionato "Al raggiungimento di una percentuale"';
+$string['form_autoplay'] = 'Autoplay';
+$string['form_autoplay_help'] = 'Abilita la riproduzione automatica quando il lettore video viene caricato';
 
 // Video types.
 $string['videotype_vimeo'] = 'Vimeo';
 $string['videotype_youtube'] = 'YouTube';
 $string['videotype_hls'] = 'HLS Stream';
 
+// Seek bar options.
+$string['seekbar_locked'] = 'Bloccata (navigazione non consentita)';
+$string['seekbar_free'] = 'Libera (navigazione completa consentita)';
+$string['seekbar_backward'] = 'Solo indietro (può riavvolgere ma non saltare avanti)';
+
+// Completion types.
+$string['completion_end'] = 'Alla fine del video';
+$string['completion_percentage'] = 'Al raggiungimento di una percentuale';
+
+// SCORM settings section.
+$string['section_scorm_settings'] = 'Impostazioni attività SCORM';
+$string['section_scorm_settings_help'] = 'Configura le impostazioni standard dell\'attività SCORM come faresti aggiungendo manualmente un\'attività SCORM a un corso';
+
 // SCORM options.
 $string['scorm_version'] = 'Versione SCORM';
 $string['scorm_version_12'] = 'SCORM 1.2';
+$string['scorm_version_2004'] = 'SCORM 2004';
 $string['scorm_maxgrade'] = 'Voto massimo';
 $string['scorm_grademethod'] = 'Metodo di valutazione';
-$string['scorm_whatgrade_high'] = 'Voto più alto';
-$string['scorm_whatgrade_first'] = 'Primo tentativo';
-$string['scorm_whatgrade_last'] = 'Ultimo tentativo';
-$string['scorm_whatgrade_avg'] = 'Voto medio';
-$string['scorm_grademethod_high'] = 'Voto più alto';
-$string['scorm_grademethod_last'] = 'Ultimo tentativo';
-$string['scorm_grademethod_first'] = 'Primo tentativo';
-$string['scorm_grademethod_avg'] = 'Voto medio';
-$string['scorm_maxattempt'] = 'Massimi tentativi';
+$string['scorm_grademethod_high'] = 'Voto più alto ottenuto';
+$string['scorm_grademethod_avg'] = 'Voto medio ottenuto';
+$string['scorm_grademethod_sum'] = 'Somma dei voti ottenuti';
+$string['scorm_grademethod_first'] = 'Voto primo tentativo';
+$string['scorm_grademethod_last'] = 'Voto ultimo tentativo';
+$string['scorm_maxattempt'] = 'Numero massimo di tentativi';
 $string['scorm_maxattempt_unlimited'] = 'Illimitati';
-$string['scorm_whatgrade'] = 'Quale voto da reportare';
-$string['scorm_whatgrade_high'] = 'Voto più alto';
+$string['scorm_whatgrade'] = 'Quale voto riportare';
+$string['scorm_whatgrade_high'] = 'Tentativo con voto più alto';
 $string['scorm_whatgrade_first'] = 'Primo tentativo';
 $string['scorm_whatgrade_last'] = 'Ultimo tentativo';
-$string['scorm_whatgrade_avg'] = 'Voto medio';
+$string['scorm_whatgrade_avg'] = 'Tentativo con voto medio';
 $string['scorm_displaycoursestructure'] = 'Visualizza struttura corso';
 $string['scorm_skipview'] = 'Salta pagina di visualizzazione';
 $string['scorm_skipview_never'] = 'Mai';
@@ -88,34 +103,31 @@ $string['scorm_skipview_first'] = 'Primo accesso';
 $string['scorm_skipview_always'] = 'Sempre';
 $string['scorm_hidebrowse'] = 'Nascondi pulsante sfoglia';
 $string['scorm_hidetoc'] = 'Nascondi tabella dei contenuti';
-$string['scorm_sidetoc_never'] = 'Mostra';
-$string['scorm_sidetoc_structure'] = 'Solo struttura';
-$string['scorm_sidetoc_all'] = 'Nascondi completamente';
-$string['scorm_nav_bar'] = 'Mostra barra di navigazione';
+$string['scorm_hidetoc_never'] = 'Mostra';
+$string['scorm_hidetoc_structure'] = 'Nascondi solo struttura';
+$string['scorm_hidetoc_all'] = 'Nascondi completamente';
+$string['scorm_nav'] = 'Mostra barra di navigazione';
 $string['scorm_auto'] = 'Auto commit';
-
-// Completion types.
-$string['completion_end'] = 'Fine del video';
-$string['completion_percentage'] = 'Al raggiungimento di una % di video';
 
 // Messages and status.
 $string['creating'] = 'Creazione del pacchetto SCORM in corso...';
 $string['success'] = 'Pacchetto SCORM creato con successo! L\'attività è stata aggiunta al corso.';
-$string['error'] = 'Errore';
-$string['error_invalid_video_url'] = 'URL video non valido o formato non supportato';
-$string['error_invalid_course'] = 'Corso non trovato o non hai il permesso di accedervi';
+$string['error'] = 'Errore durante la creazione del pacchetto SCORM';
+$string['error_invalid_video_url'] = 'URL video o formato codice non valido';
+$string['error_invalid_course'] = 'Corso selezionato non valido';
+$string['error_course_not_found'] = 'Corso non trovato o non hai il permesso di accedervi';
 $string['error_scorm_creation_failed'] = 'Impossibile creare l\'attività SCORM nel corso';
 $string['error_invalid_completion_percentage'] = 'La percentuale di completamento deve essere tra 0 e 100';
 
 // Permissions.
-$string['scormvideomaker:create'] = 'Crea pacchetti SCORM Video Maker';
-$string['scormvideomaker:manage'] = 'Gestisci pacchetti SCORM Video Maker';
+$string['scormvideomaker:create'] = 'Crea pacchetti video SCORM';
+$string['scormvideomaker:manage'] = 'Gestisci pacchetti video SCORM';
 
 // Events.
-$string['event_scorm_created'] = 'Pacchetto SCORM Video Maker creato';
-$string['event_scorm_created_desc'] = 'L\'utente ha creato un nuovo pacchetto SCORM Video Maker nel corso {$a->courseid}';
-$string['event_scorm_updated'] = 'Pacchetto SCORM Video Maker aggiornato';
-$string['event_scorm_updated_desc'] = 'L\'utente ha aggiornato un pacchetto SCORM Video Maker nel corso {$a->courseid}';
+$string['event_scorm_created'] = 'Pacchetto video SCORM creato';
+$string['event_scorm_created_desc'] = 'L\'utente ha creato un nuovo pacchetto video SCORM nel corso {courseid}';
+$string['event_scorm_updated'] = 'Pacchetto video SCORM aggiornato';
+$string['event_scorm_updated_desc'] = 'L\'utente ha aggiornato un pacchetto video SCORM nel corso {courseid}';
 
 // Privacy.
-$string['privacy:metadata'] = 'Il plugin SCORM Video Maker non memorizza alcun dato personale. Tutti i tracking e il completamento sono gestiti dal modulo SCORM standard di Moodle.'
+$string['privacy:metadata'] = 'Il plugin SCORM Video Maker non memorizza alcun dato personale. Tutti i tracking sono gestiti dal modulo attività SCORM standard.'

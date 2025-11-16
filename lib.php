6773dac2aaea6901238c3409aafcd99e4628686a
Carlo Comincini <carlo@comincini.it>
2025-11-16 02:45:07 +0100

    Sostituisci la vecchia funzione local_scormvideomaker_before_standard_html_head() con il nuovo sistema di hooks.

--- lib.php
+++ lib.php
@@ -18,7 +18,7 @@
  * Library functions for SCORM Video Maker.
  *
  * @package local_scormvideomaker
- * @copyright 2025 Carlo Comincini <carlo@comincini.it>
+ * @copyright 2025 Carlo Comincini rlo@lo@comincini.it>
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -31,86 +31,4 @@ defined('MOODLE_INTERNAL') || die();
  */
 function local_scormvideomaker_extend_navigation(global_navigation $navigation) {
     // This function can be used to add custom navigation items if needed.
-}
-
-/**
- * Hook to pre-fill SCORM mod form with our generated package.
- * This is called when the SCORM activity form is being created.
- *
- * @param MoodleQuickForm $formwrapper The form wrapper
- * @param array $data The form data
- */
-function local_scormvideomaker_coursemodule_edit_post_actions($data, $course) {
-    global $SESSION;
-    
-    // Check if we have SCORM Video Maker data in session.
-    if (isset($SESSION->local_scormvideomaker) && !empty($SESSION->local_scormvideomaker)) {
-        // Data will be used by JavaScript or by form defaults.
-        // Clean up after use.
-        $scormdata = $SESSION->local_scormvideomaker;
-        unset($SESSION->local_scormvideomaker);
-    }
-    
-    return $data;
-}
-
-/**
- * Inject custom CSS and JavaScript for SCORM form pre-filling.
- *
- * This will auto-fill the SCORM package file field when coming from our plugin.
- */
-function local_scormvideomaker_before_standard_html_head() {
-    global $SESSION, $PAGE;
-    
-    // Only run on SCORM mod edit page.
-    if ($PAGE->pagetype !== 'course-modedit' || $PAGE->url->get_param('add') !== 'scorm') {
-        return '';
-    }
-    
-    // Check if we have data from SCORM Video Maker.
-    if (!isset($SESSION->local_scormvideomaker)) {
-        return '';
-    }
-    
-    $data = $SESSION->local_scormvideomaker;
-    
-    // Inject JavaScript to pre-fill the form.
-    $js = "
-    <script>
-    require(['jquery'], function($) {
-        $(document).ready(function() {
-            // Pre-fill name field
-            if ($('#id_name').val() === '') {
-                $('#id_name').val(" . json_encode($data->name) . ");
-            }
-            
-            // Pre-fill intro editor
-            if (typeof CKEDITOR !== 'undefined') {
-                var editor = CKEDITOR.instances['id_introeditor'];
-                if (editor && editor.getData() === '') {
-                    editor.setData(" . json_encode($data->intro) . ");
-                }
-            } else if ($('#id_introeditor').length && $('#id_introeditor').val() === '') {
-                $('#id_introeditor').val(" . json_encode($data->intro) . ");
-            }
-            
-            // Set the draft item ID for the file (this is the key part)
-            if ($('input[name=\"packagefile\"]').length) {
-                $('input[name=\"packagefile\"]').val(" . json_encode($data->draftitemid) . ");
-            }
-            
-            // Show notification
-            var notification = '<div class=\"alert alert-success alert-dismissible fade show\" role=\"alert\">' +
-                '<strong>" . get_string('scorm_package_loaded', 'local_scormvideomaker') . "</strong> ' +
-                " . json_encode(get_string('scorm_package_loaded_desc', 'local_scormvideomaker')) . " +
-                '<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\">' +
-                '<span aria-hidden=\"true\">&times;</span></button></div>';
-            
-            $('#region-main').prepend(notification);
-        });
-    });
-    </script>
-    ";
-    
-    return $js;
 }
